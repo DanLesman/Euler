0f48d8f4f42bfff6c3505d18807ddad9ce672db5
Alex Lesman <alex.lesman@gmail.com>
2018-08-07 20:55:55 -0700

    improvements to p2 and adding timings

--- alex/p2.py
+++ alex/p2.py
@@ -7,23 +7,26 @@ def get_nth_fib(n):
 
     return curr
 
-n = 1
-total = 0
-while True:
-    nth_fib = get_nth_fib(n)
-    if nth_fib > 4 * 10**6:
-        break
+def compute_sum_of_evens_less_than_value(val):
+    n = 1
+    total = 0
+    while True:
+        nth_fib = get_nth_fib(n)
+        if nth_fib > val:
+            break
 
-    total += nth_fib
-    n += 3
+        total += nth_fib
+        n += 3
 
+    return total
 
-print get_nth_fib(1)
-print get_nth_fib(4)
-print total
-
-
-
+def compute_sum_of_evens_of_first_n_terms(n):
+    return sum((get_nth_fib(x) for x in xrange(1, n, 3)))
 
 
-
+import time
+for exp in xrange(1):
+    start = time.time()
+    n = 1000 * 2 ** exp
+    compute_sum_of_evens_of_first_n_terms(n)
+    print("N: {} - {}".format(n, time.time()-start))
