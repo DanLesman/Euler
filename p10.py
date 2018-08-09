c9d2ba2837671af2f5fbe54b03be532822e1ca67
Daniel Lesman <lesman.daniel@gmail.com>
2018-08-08 23:16:27 -0400

    another update

--- p10.py
+++ p10.py
@@ -1,12 +1,5 @@
 def list_primes(n):
-	#primes = [i for i in range(2,n) if is_prime(i)]
-	primestemp = [2]
-	primes = []
-	for i in range(3,n,2):
-			primestemp.append(i)
-	for j in range(0,len(primestemp)):
-		if is_prime(primestemp[j]):
-			primes.append(primestemp[j])
+	primes = [i for i in range(2,n) if is_prime(i)]
 	return primes
 
 def is_prime(n):
